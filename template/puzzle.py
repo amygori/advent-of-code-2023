5c4c0839ab1863b3b0022071e6ac7d50ec5dce44
Amy Gori <amydgori@gmail.com>
2023-12-01 11:49:30 -0500

    Initial commit

---
+++ template/puzzle.py
@@ -0,0 +1,15 @@
+import sys
+from pathlib import Path
+
+
+def do_the_thing(input):
+    return input
+
+
+if __name__ == "__main__":
+    file = Path(sys.argv[1])
+    if Path.is_file(file):
+        input = Path.read_text(file).splitlines()
+        print(do_the_thing(input))
+    else:
+        raise TypeError("This is not a file")
